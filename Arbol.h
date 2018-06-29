726fbfb281f56638ce7ca3922ab7719cf0d9f19b
NickyCA <32025754+NickyCA@users.noreply.github.com>
2018-06-29 19:06:16 +1200

    Add files via upload

--- Arbol.h
+++ Arbol.h
@@ -3,6 +3,7 @@
 #include "Par.h"
 #include <vector> // para el iterador
 
+template< class T1, class T2 >
 class Arbol{
 	private:
 	 class Nodo{
@@ -10,33 +11,36 @@ class Arbol{
 		Nodo * izq;
 		Nodo * der;
 		char color;
-		Par * par;
-	    Nodo(Par *);
+		Par<T1,T2> * par;
+	    Nodo(Par<T1,T2> *);
 		~Nodo();
-	    void insertar(Par *);
+	    void insertar(Par<T1,T2> *);
 		
 	 };
-	 Nodo * bisabuelo;
-	 Nodo * abuelo;
+	 Nodo * padre;
 	 Nodo * raiz;
 	public:
 	
-	class Iterador{
-		 
-		
-	}
+	
 	
      Arbol();
 	~Arbol();
-    int tipoRotacion(); // 2 si es simple 1 si es doble
-    bool esRN(); // si es rojo negro no necesita rotar modifique punteros a padre y abuelo
+    int tipoRotacion(Nodo *, int); // recibe el nodo padre,2 si es simple 1 si es doble (aqui se calcula el lado)
+    Nodo* esRN(Nodo *); // recibe el lado donde empieza(nodoizq o nodoDer) 
+	void buscarPadre(Nodo * );
+	int negroRojos(Nodo *);
     bool hayCFlip(Nodo *); // es color flip
-	void rotacionSimple();
-	void rotacionDoble();
+	void rotacionSimple(Nodo *, int);//nodo * va a ser el padre de donde esta el problema y el int especifica el lado del hijo problema
+	void rotacionDoble(Nodo *, int);//nodo * va a ser el padre de donde esta el problema y el int especifica el lado del hijo problema
 	void recoloreo(Nodo *); //lo llama cada rotacion
 	void cambioRaiz();
 	void colorFlip(Nodo *);
 	void insertar(const char *);
+	int esHoja(Nodo &);//sirve para cuando se quiere imprimir
+	
+		
+		
 	
 	
-}
+}; 
+#endif
