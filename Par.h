30199de49f826ab679418e996a6676ce1ba66f46
NickyCA <32025754+NickyCA@users.noreply.github.com>
2018-06-30 12:34:13 +1200

    Add files via upload

--- Par.h
+++ Par.h
@@ -1,22 +1,70 @@
 #ifndef PAR_EMPLANTILLADO
 #define PAR_EMPLANTILLADO
 #include<iostream>
+#include <string>
 using namespace std;
 template< class T1, class T2 >
 class Par {
 	friend ostream& operator<<(ostream& salida, Par<T1,T2> par){
-		salida << "("<< par.x << "," << par.y << ")"<< endl;
+		salida << "("<< par.llave << "," << par.dato<< ")"<< endl;
 		return salida;
 	}
 	
+	friend istream& operator<<(istream& entrada, Par<T1,T2> * par){
+		par->cargar(entrada);
+		return entrada;
+	}
+	
 	private:
-  	  T1 x;
-	  T2 y;
+  	  T1 llave;
+	  T2 dato;
 	public:
-	  Par(T1 x, T2 y){
-		this->x = x;
-        this->y = y;		  
+	  Par(T1 llave, T2 dato){
+		this->llave = llave;
+        this->dato = dato;		  
 	  }
+	  
+	  Par(Par* par){
+		  this->llave= par->llave;
+		  this->dato = par->dato;
+		  
+	  }
+	  
+	  istream& cargar(istream& entrada){
+		  entrada>>llave;
+		  entrada>>dato;
+		  return entrada;
+	  }
+	  
+	  int operator> (const Par<T1,T2> * par){		 
+		  if(this->llave >= par->llave){
+			  return 1;
+			  
+		  }
+		  else{
+			  return 0;
+		  }
+	  }
+
+		T1 getLlave(){
+			return llave;
+			
+		}
+		
+		void setDato(T2 dato){
+			this->dato=dato;
+		}
+		
+		void setLlave(T1 llave){
+			this->llave=llave;
+			
+		}
+	  
+	
+	  
+	  
+	  
+	  
 };
 
 //comparar cons copia destructor cargar sobrecargado
