794fb7fab484e5af8c9e7658b8bf77fe2afaf99a
NickyCA <32025754+NickyCA@users.noreply.github.com>
2018-07-04 14:51:26 -0600

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,12 @@
 #include "Arbol.h"
 #include "Par.h"
 #include <iostream>
-
 using namespace std;
+int main(int argc, char **argv){
 
-int main(){
+Arbol<char,double> arbol;
+arbol.insertar(argv[1]);
+cout<<arbol<<endl<<endl;
 
-  Arbol<int,char> arbol;
-  
-for(int i=1; i<10; ++i){
-	arbol.insertar(new Par<int,char> (i,'c'));
-	cout<<arbol<<endl<<endl;	
-}
-
-
-   
-   return 0;	
+return 0;
 }
